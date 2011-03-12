2fac50f68d984c66c0a1d8013504281a815602b1
Jiv Dhaliwal <jivdhaliwal@gmail.com>
2011-03-12 19:03:14 +0000

    GameplayState added

---
+++ src/towerdefence/State/Game.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package towerdefence.State;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame
+{
+     static int height = 18*32;
+     static int width = 18*32;
+
+     static boolean fullscreen = false;
+
+     static boolean showFPS = true;
+
+     static String title = "Basic Game Template";
+
+     static int fpslimit = 60;
+
+     public static final int MAINMENUSTATE = 0;
+     public static final int GAMEPLAYSTATE = 1;
+
+     
+
+     public Game(String title)
+     {
+          super(title);
+
+          //this.addState(new MainMenuState(MAINMENUSTATE));
+          this.addState(new GameplayState(GAMEPLAYSTATE));
+          this.enterState(GAMEPLAYSTATE);
+     }
+
+     public static void main(String[] args) throws SlickException
+     {
+          AppGameContainer app = new AppGameContainer(new ScalableGame(new Game(title),width,height));
+           //=new AppGameContainer(new Game(title));
+
+          app.setDisplayMode((int)(width), (int)(height), fullscreen);
+          app.setSmoothDeltas(true);
+          app.setTargetFrameRate(fpslimit);
+          app.setShowFPS(showFPS);
+          app.start();
+     }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {
+        //this.getState(MAINMENUSTATE).init(container, this);
+        this.getState(GAMEPLAYSTATE).init(container, this);
+    }
+}
