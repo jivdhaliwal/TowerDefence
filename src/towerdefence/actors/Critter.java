0d7e4434cf4fa942cfba1d28a13253496cbe0065
Jiv Dhaliwal <jivdhaliwal@gmail.com>
2011-03-14 14:45:12 +0000

    Added entity manager and refactored packages

---
+++ src/towerdefence/actors/Critter.java
@@ -0,0 +1,12 @@
+package towerdefence.actors;
+
+/**
+ *
+ * @author Jiv Dhaliwal <jivdhaliwal@gmail.com>
+ */
+
+import towerdefence.base.Component;
+
+public class Critter {
+
+}
---
+++ src/towerdefence/base/RenderComponent.java
@@ -0,0 +1,22 @@
+package towerdefence.base;
+
+/**
+ *
+ * @author Jiv Dhaliwal <jivdhaliwal@gmail.com>
+ */
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public abstract class RenderComponent extends Component {
+
+    public RenderComponent(String id)
+    {
+        this.id = id;
+    }
+
+    public abstract void render(GameContainer gc, StateBasedGame sb, Graphics gr);
+
+}
