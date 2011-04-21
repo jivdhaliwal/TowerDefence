788a89cb36bffeeab9850036571fc87794506062
Shuo Chen <nebaries@gmail.com>
2011-04-21 16:05:13 +0100

    linked 2 things together

--- src/cuda/CudaCritterSelector.java
+++ src/cuda/CudaCritterSelector.java
@@ -1,247 +1,44 @@
 package cuda;
 
-import java.io.*;
-
-import jcuda.*;
 import jcuda.driver.*;
 
-public class CudaCritterSelector {
-  private CUmodule module;
-  public CudaCritterSelector(String filename) throws Exception {
-    String cubinFileName = prepareCubinFile(filename);
+public class cudaCritterSelector {
+  private DistanceCalc dc;
+   Reduction rd;
+  
+  public cudaCritterSelector() {
     JCudaDriver.cuInit(0);
     CUcontext pctx = new CUcontext();
     CUdevice dev = new CUdevice();
     JCudaDriver.cuDeviceGet(dev, 0);
     JCudaDriver.cuCtxCreate(pctx, 0, dev);
-
-
-    // Load the CUBIN file.
-    module = new CUmodule();
-    JCudaDriver.cuModuleLoad(module, cubinFileName);
+    try {
+      rd = new Reduction("reduction.cu");
+      dc = new DistanceCalc("CUDA_Distance.cu");
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
   }
   
-  public void calcDist(int critters[][], int towers[][], int threadPerBlock) {
-
-    CUfunction function = new CUfunction();
-    JCudaDriver.cuModuleGetFunction(function, module, "calcDistance");
-
-
-
-    int numThreads = critters.length;
-    
-    int numBlocks = (int) Math.ceil((double)numThreads / threadPerBlock);
-
-    int numTowers = towers.length;
-    int size = critters[0].length;
-
-    // Allocate and fill host input memory: A 2D int array with
-    // 'numThreads' rows and 'size' columns, each row filled with
-    // the values from 0 to size-1.
-    CUdeviceptr hostDevicePointers[] = new CUdeviceptr[numThreads];
-    for(int i = 0; i < numThreads; i++) {
-        hostDevicePointers[i] = new CUdeviceptr();
-        JCudaDriver.cuMemAlloc(hostDevicePointers[i], size * Sizeof.INT);
-        
-        JCudaDriver.cuMemcpyHtoD(hostDevicePointers[i],
-            Pointer.to(critters[i]), size * Sizeof.INT);
-    }
-    
-    CUdeviceptr queryHostDevicePointers[] = new CUdeviceptr[numTowers];
-    for (int i = 0; i < numTowers; i++) {
-      queryHostDevicePointers[i] = new CUdeviceptr();
-      JCudaDriver.cuMemAlloc(queryHostDevicePointers[i], size * Sizeof.INT);
-      
-      JCudaDriver.cuMemcpyHtoD(queryHostDevicePointers[i],
-          Pointer.to(towers[i]), size * Sizeof.INT);
-    }
-    
-    
-    // Allocate device memory for the array pointers, and copy
-    // the array pointers from the host to the device.
-    CUdeviceptr deviceInput = new CUdeviceptr();
-    CUdeviceptr deviceQuery = new CUdeviceptr();
-    
-    JCudaDriver.cuMemAlloc(deviceInput, numThreads * Sizeof.POINTER);
-    JCudaDriver.cuMemcpyHtoD(deviceInput, Pointer.to(hostDevicePointers),
-        numThreads * Sizeof.POINTER);
-
-    
-    JCudaDriver.cuMemAlloc(deviceQuery, numTowers * Sizeof.POINTER);
-    JCudaDriver.cuMemcpyHtoD(deviceQuery, Pointer.to(queryHostDevicePointers), numTowers * Sizeof.POINTER);
-    
+  public int[] selectCritters(int critters[][], int towers[][], int range) {
     CUdeviceptr deviceOutput = new CUdeviceptr();
-    JCudaDriver.cuMemAlloc(deviceOutput, numTowers * numThreads * 2 * Sizeof.INT);
-    
-    JCudaDriver.cuFuncSetBlockShape(function, Math.min(threadPerBlock, numThreads), 1, 1);
-    
-    // Set up the parameters for the function call: One pointer (to
-    // pointers) for the input, one int for the size, and one pointer
-    // for the output array. Note that for 'cuParamSetv' you have
-    // to pass a pointer to a pointer, in order to set the value
-    // of the pointer as the parameter.
-    Pointer dIn = Pointer.to(deviceInput);
-    
-    Pointer dOut = Pointer.to(deviceOutput);
-    Pointer pSize = Pointer.to(new int[]{size});
-    Pointer pN    = Pointer.to(new int[]{numThreads});
-    
-    Pointer dQu = Pointer.to(deviceQuery);
-    
-
-    int offset = 0;
-    offset = JCudaDriver.align(offset, Sizeof.POINTER);
-    JCudaDriver.cuParamSetv(function, offset, dIn, Sizeof.POINTER);
-    offset += Sizeof.POINTER;
-
-    offset = JCudaDriver.align(offset, Sizeof.INT);
-    JCudaDriver.cuParamSetv(function, offset, pSize, Sizeof.INT);
-    offset += Sizeof.INT;
-    
-    offset = JCudaDriver.align(offset, Sizeof.INT);
-    JCudaDriver.cuParamSetv(function, offset, pN, Sizeof.INT);
-    offset += Sizeof.INT;
-    
-    offset = JCudaDriver.align(offset, Sizeof.POINTER);
-    JCudaDriver.cuParamSetv(function, offset, dQu, Sizeof.POINTER);
-    offset += Sizeof.POINTER;
-    
-
-    offset = JCudaDriver.align(offset, Sizeof.POINTER);
-    JCudaDriver.cuParamSetv(function, offset, dOut, Sizeof.POINTER);
-    offset += Sizeof.POINTER;
-
-    JCudaDriver.cuParamSetSize(function, offset);
-    
-    JCudaDriver.cuLaunchGrid(function, numBlocks, numTowers);
-    JCudaDriver.cuCtxSynchronize();
-    
-    
-    int hostOutput[] = new int[numTowers * numThreads * 2];
-    JCudaDriver.cuMemcpyDtoH(Pointer.to(hostOutput), deviceOutput,
-        numTowers * numThreads * 2 * Sizeof.INT);
- 
-    
-    for (int output : hostOutput)
-      System.out.println(output);
-    
-    for(int i = 0; i < numThreads; i++)
-    {
-        JCudaDriver.cuMemFree(hostDevicePointers[i]);
-    }
-    for (int i = 0; i < numTowers; i++) {
-      JCudaDriver.cuMemFree(queryHostDevicePointers[i]);
-    }
-    JCudaDriver.cuMemFreeHost(dIn);
-    JCudaDriver.cuMemFreeHost(dOut);
-    JCudaDriver.cuMemFreeHost(pSize);
-    JCudaDriver.cuMemFreeHost(pN);
-    JCudaDriver.cuMemFreeHost(dQu);
-   
-    
-    JCudaDriver.cuMemFree(deviceInput);
-    JCudaDriver.cuMemFree(deviceQuery);
-    JCudaDriver.cuMemFree(deviceOutput);
-    
-  }
-  
-  
-  /**
-   * The extension of the given file name is replaced with "cubin".
-   * If the file with the resulting name does not exist, it is
-   * compiled from the given file using NVCC. The name of the
-   * cubin file is returned.
-   *
-   * @param cuFileName The name of the .CU file
-   * @return The name of the CUBIN file
-   * @throws IOException If an I/O error occurs
-   */
-  private static String prepareCubinFile(String cuFileName) throws IOException
-  {
-      int endIndex = cuFileName.lastIndexOf('.');
-      if (endIndex == -1)
-      {
-          endIndex = cuFileName.length()-1;
-      }
-      String cubinFileName = cuFileName.substring(0, endIndex+1)+"cubin";
-      File cubinFile = new File(cubinFileName);
-      if (cubinFile.exists())
-      {
-          //return cubinFileName;
-      }
-
-      File cuFile = new File(cuFileName);
-      if (!cuFile.exists())
-      {
-          throw new IOException("Input file not found: "+cuFileName);
-      }
-      String modelString = "-m"+System.getProperty("sun.arch.data.model");
-      String command =
-          "/usr/local/cuda/bin/nvcc " + modelString + " -arch sm_11 -cubin "+
-          cuFile.getPath()+" -o "+cubinFileName;
-
-      System.out.println("Executing\n"+command);
-      Process process = Runtime.getRuntime().exec(command);
-
-      String errorMessage = new String(toByteArray(process.getErrorStream()));
-      String outputMessage = new String(toByteArray(process.getInputStream()));
-      int exitValue = 0;
-      try
-      {
-          exitValue = process.waitFor();
-      }
-      catch (InterruptedException e)
-      {
-          Thread.currentThread().interrupt();
-          throw new IOException("Interrupted while waiting for nvcc output", e);
-      }
-
-      System.out.println("nvcc process exitValue "+exitValue);
-      if (exitValue != 0)
-      {
-          System.out.println("errorMessage:\n"+errorMessage);
-          System.out.println("outputMessage:\n"+outputMessage);
-          throw new IOException("Could not create .cubin file: "+errorMessage);
-      }
-      return cubinFileName;
-  }
-
-  /**
-   * Fully reads the given InputStream and returns it as a byte array.
-   *
-   * @param inputStream The input stream to read
-   * @return The byte array containing the data from the input stream
-   * @throws IOException If an I/O error occurs
-   */
-  private static byte[] toByteArray(InputStream inputStream) throws IOException
-  {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      byte buffer[] = new byte[8192];
-      while (true)
-      {
-          int read = inputStream.read(buffer);
-          if (read == -1)
-          {
-              break;
-          }
-          baos.write(buffer, 0, read);
-      }
-      return baos.toByteArray();
+    dc.calcDist(critters, towers, 512, deviceOutput);
+    return rd.reduction(deviceOutput, critters.length, towers.length, range);
   }
   
   
   public static void main(String[] args) throws Exception {
     
-    int hostInput[][] = new int[11][2];
-    for(int i = 0; i < 11; i++)
+    int hostInput[][] = new int[2000][2];
+    for(int i = 0; i < 2000; i++)
     {
         for (int j=0; j<2; j++)
         {
-            hostInput[i][j] = j + i + 1;
+            hostInput[i][j] = (int)(Math.random() * 100);
         }
     }
     
-    int towerNo = 3;
+    int towerNo = 20;
     
     int hostQuery[][] = new int[towerNo][2];
     for (int i = 0; i < towerNo; i++) {
@@ -249,14 +46,9 @@ public class CudaCritterSelector {
         hostQuery[i][j] = (i + j * j + 1);
       }
     }
-    
+    cudaCritterSelector c = new cudaCritterSelector();
+    for (int i : c.selectCritters(hostInput, hostQuery, 10))
+      System.out.println(i);
 
-    CudaCritterSelector c = new CudaCritterSelector("CUDA_Distance.cu");
-    c.calcDist(hostInput, hostQuery, 5);
   }
-  
-  
-  
-  
-  
 }
--- src/cuda/Reduction.java
+++ src/cuda/Reduction.java
@@ -1,111 +1,16 @@
 package cuda;
-import java.io.*;
 
 import jcuda.*;
 import jcuda.driver.*;
 
-public class Reduction {
-  private CUmodule module;
+public class Reduction extends cudaKernel {
   public Reduction(String filename) throws Exception {
-    String cubinFileName = prepareCubinFile(filename);
-    JCudaDriver.cuInit(0);
-    CUcontext pctx = new CUcontext();
-    CUdevice dev = new CUdevice();
-    JCudaDriver.cuDeviceGet(dev, 0);
-    JCudaDriver.cuCtxCreate(pctx, 0, dev);
-
-
-    // Load the CUBIN file.
-    module = new CUmodule();
-    JCudaDriver.cuModuleLoad(module, cubinFileName);
-  }
-  private static String prepareCubinFile(String cuFileName) throws IOException
-  {
-      int endIndex = cuFileName.lastIndexOf('.');
-      if (endIndex == -1)
-      {
-          endIndex = cuFileName.length()-1;
-      }
-      String cubinFileName = cuFileName.substring(0, endIndex+1)+"cubin";
-      File cubinFile = new File(cubinFileName);
-      if (cubinFile.exists())
-      {
-          //return cubinFileName;
-      }
-
-      File cuFile = new File(cuFileName);
-      if (!cuFile.exists())
-      {
-          throw new IOException("Input file not found: "+cuFileName);
-      }
-      String modelString = "-m"+System.getProperty("sun.arch.data.model");
-      String command =
-          "/usr/local/cuda/bin/nvcc " + modelString + " -arch sm_11 -cubin "+
-          cuFile.getPath()+" -o "+cubinFileName;
-
-      System.out.println("Executing\n"+command);
-      Process process = Runtime.getRuntime().exec(command);
-
-      String errorMessage = new String(toByteArray(process.getErrorStream()));
-      String outputMessage = new String(toByteArray(process.getInputStream()));
-      int exitValue = 0;
-      try
-      {
-          exitValue = process.waitFor();
-      }
-      catch (InterruptedException e)
-      {
-          Thread.currentThread().interrupt();
-          throw new IOException("Interrupted while waiting for nvcc output", e);
-      }
-
-      System.out.println("nvcc process exitValue "+exitValue);
-      if (exitValue != 0)
-      {
-          System.out.println("errorMessage:\n"+errorMessage);
-          System.out.println("outputMessage:\n"+outputMessage);
-          throw new IOException("Could not create .cubin file: "+errorMessage);
-      }
-      return cubinFileName;
-  }
-
-  /**
-   * Fully reads the given InputStream and returns it as a byte array.
-   *
-   * @param inputStream The input stream to read
-   * @return The byte array containing the data from the input stream
-   * @throws IOException If an I/O error occurs
-   */
-  private static byte[] toByteArray(InputStream inputStream) throws IOException
-  {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      byte buffer[] = new byte[8192];
-      while (true)
-      {
-          int read = inputStream.read(buffer);
-          if (read == -1)
-          {
-              break;
-          }
-          baos.write(buffer, 0, read);
-      }
-      return baos.toByteArray();
+    super(filename);
   }
   
-  
-  
-  public void reduction(int[] input, int towers) {
-
-    
-    int size = input.length / 2 / towers;
-    
+  public int[] reduction(CUdeviceptr deviceInput, int size, int towers, int range) {    
     int threadNum = Math.min(size, 512);
     int blockNum  = (int) Math.ceil((double)size / 512); 
-    
-    CUdeviceptr deviceInput = new CUdeviceptr();
-    JCudaDriver.cuMemAlloc(deviceInput, size * towers * 2 * Sizeof.INT);
-    JCudaDriver.cuMemcpyHtoD(deviceInput,
-      Pointer.to(input), size * towers * 2 * Sizeof.INT);
     
     CUdeviceptr deviceOutput = new CUdeviceptr();
     JCudaDriver.cuMemAlloc(deviceOutput, towers * blockNum * 2 * Sizeof.INT);
@@ -135,13 +40,20 @@ public class Reduction {
     JCudaDriver.cuMemcpyDtoH(Pointer.to(hostOutput), deviceOutput, towers * blockNum * 2 * Sizeof.INT);
     JCudaDriver.cuMemFree(deviceOutput);
     
-    for (int result : hostOutput)
-      System.out.println(result);
+    int result[] = new int[towers];
+    for (int i = 0; i < towers; i ++) {
+      if (hostOutput[i * 2] <= range)
+        result[i] = hostOutput[i * 2 + 1];
+      else
+        result[i] = -1;
+    }
+    return result;
+    
   }
   
   private void launchKernel(CUdeviceptr deviceInput, CUdeviceptr deviceOutput, int n, int threadNum, int blockNumX, int blockNumY) {
     CUfunction function = new CUfunction();
-    JCudaDriver.cuModuleGetFunction(function, module, "reduce");
+    JCudaDriver.cuModuleGetFunction(function, getModule(), "reduce");
     
     Pointer dIn  = Pointer.to(deviceInput);
     Pointer dOut = Pointer.to(deviceOutput);
@@ -163,6 +75,7 @@ public class Reduction {
     JCudaDriver.cuParamSetSize(function, offset);
     JCudaDriver.cuFuncSetBlockShape(function, threadNum, 1, 1);
     JCudaDriver.cuFuncSetSharedSize(function, threadNum * 2 * Sizeof.INT);
+ 
     
     JCudaDriver.cuLaunchGrid(function, blockNumX, blockNumY);
     JCudaDriver.cuCtxSynchronize();
@@ -174,18 +87,5 @@ public class Reduction {
     JCudaDriver.cuMemFree(deviceInput);
   }
   
-  public static void main(String[] args) throws Exception {
-//    int threads = 66;
-//    int inputs[] = new int[threads * 2];
-//    for (int i = 0; i < threads; i ++)
-//      inputs[i * 2] = threads - i; //(int)(Math.random() * 100);
-//   
-//    for (int i = 0; i < threads; i ++)
-//      inputs[i * 2 + 1] = i % (threads / 2);
-//   
-    int inputs[] = new int[]{0, 0, 2, 1, 8, 2, 18, 3, 32, 4, 50, 5, 72, 6, 98, 7, 128, 8, 162, 9, 200, 10, 2, 0, 0, 1, 2, 2, 8, 3, 18, 4, 32, 5, 50, 6, 72, 7, 98, 8, 128, 9, 162, 10, 8, 0, 2, 1, 0, 2, 2, 3, 8, 4, 18, 5, 32, 6, 50, 7, 72, 8, 98, 9, 128, 10};
-    Reduction r = new Reduction("reduction.cu");
-    r.reduction(inputs, 3);
-  }
   
 }
