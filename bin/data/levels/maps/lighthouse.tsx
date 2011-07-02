2514c66934757336354d428a7103dd7e72582d84
Jiv Dhaliwal <jivdhaliwal@gmail.com>
2011-07-02 16:03:15 +0100

    Cleaning up

--- bin/data/levels/maps/lighthouse.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="lighthouse" tilewidth="32" tileheight="32">
- <image source="lighthouse.png" width="72" height="72"/>
-</tileset>
--- bin/data/levels/maps/tilesetgrassland2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetgrassland2" tilewidth="32" tileheight="32">
- <image source="tilesetgrassland2.png" trans="000000" width="869" height="474"/>
-</tileset>
